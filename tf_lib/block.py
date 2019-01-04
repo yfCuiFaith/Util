933250f3ad7f87dcd82c8d7e97e1ccda39b8b444
yfCuiFaith <yfcui_ecnu@163.com>
2019-01-04 16:37:56 +0800

    Update block.py

--- tf_lib/block.py
+++ tf_lib/block.py
@@ -44,6 +44,52 @@ def BaseBlock(input_tensor, filter_shape, bn_shape, is_test, g_step, \
 	
 	return block_concat2
 
+def PlainBlock(input_tensor, filter_shape, is_test, g_step, \
+		update_ema_gp, block_name):
+	with tf.name_scope(block_name):
+		input_tensor_shape = input_tensor.get_shape().as_list()
+		batch_size = input_tensor_shape[0]
+
+		padding = filter_shape[0] / 2
+
+		conv_name = '%s_conv' % block_name
+		ln_name = '%s_ln' % block_name
+
+		block_input = tf.reshape(input_tensor, [-1, input_tensor_shape[3]])
+		block_ln = LayerNorm(block_input, scope = ln_name)
+		block_ln = tf.reshape(block_ln, [batch_size, -1, 1, input_tensor_shape[3]])
+		block_glu = GLU(block_ln)
+
+		block_glu = tf.pad(block_glu, [[0, 0], [padding, padding], [0, 0], [0, 0]])
+
+		block_conv = Conv(block_glu, filter_shape, name = conv_name)
+
+	return block_conv
+
+def ResidualBlock(input_tensor, filter_shape, is_test, g_step, \
+		update_ema_gp, block_name):
+	with tf.name_scope(block_name):
+		input_tensor_shape = input_tensor.get_shape().as_list()
+		batch_size = input_tensor_shape[0]
+
+		padding = filter_shape[0] / 2
+
+		conv_name = '%s_conv' % block_name
+		ln_name = '%s_ln' % block_name
+
+		block_input = tf.reshape(input_tensor, [-1, input_tensor_shape[3]])
+		block_ln = LayerNorm(block_input, scope = ln_name)
+		block_ln = tf.reshape(block_ln, [batch_size, -1, 1, input_tensor_shape[3]])
+		block_glu = GLU(block_ln)
+		
+		block_glu = tf.pad(block_glu, [[0, 0], [padding, padding], [0, 0], [0, 0]])
+
+		block_conv = Conv(block_glu, filter_shape, name = conv_name)
+
+		block_output = input_tensor + block_conv
+	return block_output
+
+
 '''
 def ResidualBlock(input_tensor, filter_shape, is_test, g_step, \
 		reg_list, update_ema_gp, block_name):
@@ -74,31 +120,74 @@ def ResidualBlock(input_tensor, filter_shape, is_test, g_step, \
 	return block_relu2
 '''
 
-def ResidualBlock(input_tensor, filter_shape, is_test, g_step, \
-		reg_list, update_ema_gp, block_name):
+def NormBlock(input_tensor, is_test, g_step, update_ema_gp, block_name):
 	with tf.name_scope(block_name):
-		bn_shape = [1, 1, filter_shape[3]]
+		input_tensor_shape = input_tensor.get_shape().as_list()
+		batch_size = input_tensor_shape[0]
 
-		#conv1_input = tf.reshape(input_tensor, [1, -1, filter_shape[1], filter_shape[2]])
-		conv1_input = tf.pad(input_tensor, [[0, 0], [1, 1], [0, 0], [0, 0]])
+		conv_name = '%s_conv' % block_name
+		ln_name = '%s_ln' % block_name
 
-		block_conv1 = Conv(conv1_input, filter_shape, reg_list = reg_list, name = 'conv1')
-		block_conv1 = tf.reshape(block_conv1, [-1, filter_shape[3]])
-		block_ln1 = LayerNorm(block_conv1, 'ln1')
-		block_ln1 = tf.reshape(block_ln1, [1, -1, 1, filter_shape[3]])
-		#block_relu1 = tf.nn.relu(block_ln1)
-		block_glu1 = GLU(block_ln1)
-		block_concat1 = block_glu1 + input_tensor
+		block_input = tf.reshape(input_tensor, [-1, input_tensor_shape[3]])
+		block_ln = LayerNorm(block_input, scope = ln_name)
+		block_ln = tf.reshape(block_ln, [batch_size, -1, 1, input_tensor_shape[3]])
+		block_glu = GLU(block_ln)
+	return block_glu
 
-		#conv2_input = tf.reshape(block_relu1, [1, -1, filter_shape[1], filter_shape[2]])
-		conv2_input = tf.pad(block_concat1, [[0, 0], [1, 1], [0, 0], [0, 0]])
+def StageBlock(input_tensor, filter_shape, layer_num, is_last, \
+		is_test, g_step, update_ema_gp, block_name):
+	with tf.name_scope(block_name):
+		buffer_tensor = input_tensor
 
-		block_conv2 = Conv(conv2_input, filter_shape, reg_list = reg_list, name = 'conv2')
-		block_conv2 = tf.reshape(block_conv2, [-1, filter_shape[3]])
-		block_ln2 = LayerNorm(block_conv2, 'ln2')
-		block_ln2 = tf.reshape(block_ln2, [1, -1, 1, filter_shape[3]])
-		#block_relu2 = tf.nn.relu(block_concat)
-		block_glu2 = GLU(block_ln2)
-		block_concat2 = block_glu2 + block_concat1
-	
-	return block_glu2
+		for i in range(layer_num):
+			sub_block_name = str.format('%s_block%d' % (block_name, i))
+
+			buffer_tensor = \
+				ResidualBlock(buffer_tensor, filter_shape, \
+					is_test, g_step, update_ema_gp, sub_block_name)
+			print(buffer_tensor)
+		if(is_last):
+			buffer_tensor = \
+				NormBlock(buffer_tensor, is_test, g_step, \
+					update_ema_gp, '%s_top' % (block_name))
+		else:
+			buffer_tensor = \
+				PlainBlock(buffer_tensor, filter_shape, is_test, \
+					g_step, update_ema_gp, '%s_top' % (block_name))
+		print(buffer_tensor)
+	return buffer_tensor
+
+def DevPlainBlock(input_tensor, filter_shape, direction, block_name):
+	with tf.name_scope(block_name):
+		input_tensor_shape = input_tensor.get_shape().as_list()
+		batch_size = input_tensor_shape[0]
+
+		conv_name = '%s_conv' % block_name
+		ln_name = '%s_ln' % block_name
+
+		block_input = tf.reshape(input_tensor, [-1, input_tensor_shape[3]])
+		block_ln = LayerNorm(block_input, scope = ln_name)
+		block_ln = tf.reshape(block_ln, [batch_size, -1, 1, input_tensor_shape[3]])
+		block_glu = GLU(block_ln)
+
+		block_conv = DevConv(block_glu, filter_shape, direction, name = conv_name)
+
+	return block_conv
+
+def DevResidualBlock(input_tensor, filter_shape, direction, block_name):
+	with tf.name_scope(block_name):
+		input_tensor_shape = input_tensor.get_shape().as_list()
+		batch_size = input_tensor_shape[0]
+
+		conv_name = '%s_conv' % block_name
+		ln_name = '%s_ln' % block_name
+
+		block_input = tf.reshape(input_tensor, [-1, input_tensor_shape[3]])
+		block_ln = LayerNorm(block_input, scope = ln_name)
+		block_ln = tf.reshape(block_ln, [batch_size, -1, 1, input_tensor_shape[3]])
+		block_glu = GLU(block_ln)
+		
+		block_conv = DevConv(block_glu, filter_shape, direction, name = conv_name)
+
+		block_output = input_tensor + block_conv
+	return block_output
