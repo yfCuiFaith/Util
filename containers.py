48d6106b5b4aa41586bf3cecbdc6ae0c52744fa1
yfCuiFaith <yfcui_ecnu@163.com>
2019-01-04 16:41:44 +0800

    Update and rename dataset.py to containers.py

--- containers.py
+++ containers.py
@@ -1,3 +1,4 @@
+import numpy as np
 import gc
 
 class DataSet:
@@ -240,6 +241,12 @@ class SeqDataSet:
 		else:
 			self.number = len(self.label)
 
+	def Number(self):
+		return self.number
+
+	def Lengths(self):
+		return [len(seq) for seq in self.label]
+	
 	def Fill(self, src_seqs, filler):
 		seqs_lens = [len(seq) for seq in src_seqs]
 
@@ -292,10 +299,9 @@ class SeqDataSet:
 	def SetSignal(self):
 		self.avail_num = self.number
 
-	# |dataset| % batch_size doesn't have to be 0
+	# |dataset| % batch_size must be 0
 	def NextRestrictedBatch(self, batch_size = 1):
 		if(self.avail_num > 0):
-			batch_size = min(self.avail_num, batch_size)
 			self.avail_num -= batch_size
 			return self.NextBatch(batch_size)
 		return [None, None]
@@ -309,3 +315,45 @@ class SeqDataSet:
 
 	def EntireBatch(self):
 		return self.feature, self.label
+
+class HistoryPool:
+	def __init__(self, init_lengths):
+		self.index = 0
+		self.pool = []
+		self.lengths = init_lengths
+		self.size = len(init_lengths)
+
+		for i in range(len(init_lengths)):
+			self.pool.append([[[0.0, 0.0]] for j in range(init_lengths[i])])
+	
+	def Forget(self):
+		for i in range(len(self.pool)):
+			self.pool[i] = [[[0.0, 0.0]] for j in range(len(self.pool[i]))]
+
+	def Update(self, update_tensor):
+		batch_size = len(update_tensor)
+		for i in range(0, batch_size):
+			self.pool[self.index] = update_tensor[i][:self.lengths[self.index]]
+			self.index = (self.index + 1) % self.size
+
+	def Get(self, batch_size, filler):
+		lengths = [len(self.pool[i % self.size]) \
+			for i in range(self.index, self.index + batch_size)]
+		max_len = max(lengths)
+
+		res = []
+		for i in range(batch_size):
+			pad_size = max_len - lengths[i]
+
+			prim_tensor = self.pool[(self.index + i) % self.size]
+			padding = [filler for j in range(pad_size)]
+
+			if(isinstance(prim_tensor, np.ndarray)):
+				prim_tensor = prim_tensor.tolist()
+
+			res.append(prim_tensor + padding)
+
+		return res
+
+	def GoAhead(self, batch_size):
+		self.index = (self.index + batch_size) % self.size
